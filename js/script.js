fc7e08ee131e38c97f7f90691cf9f7455a7842e0
eslam shosha <you@example.comeslamshosha22.es@gmail.com>
2022-02-06 17:04:43 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+	new WOW().init();
+
+	//phone size menu onclick
+	if ($(window).width() <= 1199) {
+        $('#menu-id').click(function (e) {
+           e.preventDefault()
+           $(".overlay-box").fadeIn(300);
+           $(".navgition").addClass("reset-left");
+           $("body").addClass("overflow");
+       });
+       $(".nav-head .close-btn, .overlay-box").click(function () {
+           $(".overlay-box").fadeOut(300);
+           $(".navgition").removeClass("reset-left");
+           $("body").removeClass("overflow");
+       });
+   	}
+
+    $(".new-address-select").select2();
+});
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+		
+	
+		
+
